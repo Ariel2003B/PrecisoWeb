881c377d2ba442194b10b8c7b7edf52c2284f5b8
Elvis Guato <elvisguato02@gmail.com>
2024-12-24 14:25:04 -0500

    correcciones en subir SIMCards y pop navideno 4

--- back/resources/views/home/index.blade.php
+++ back/resources/views/home/index.blade.php
@@ -4,9 +4,9 @@
 @section('content')
     <header class="masthead">
         <!-- <video autoplay muted loop class="masthead-video">
-                        <source src="/assets/img/AdobeStock_300771413.mov" type="video/mp4">
-                        Tu navegador no soporta la reproducción de video.
-                    </video> -->
+                            <source src="/assets/img/AdobeStock_300771413.mov" type="video/mp4">
+                            Tu navegador no soporta la reproducción de video.
+                        </video> -->
         <video autoplay muted loop class="masthead-video">
             <source src="img/FondoNavidad.mp4" type="video/mp4">
             Tu navegador no soporta la reproducción de video.
@@ -22,8 +22,58 @@
         </div>
 
         <!-- <div id="contador-visitas" class="contador">
-                        
-                    </div> -->
+                            
+                        </div> -->
     </header>
+
+
+    <div class="modal-navidad">
+        <div class="modal-content-navidad">
+            <div class="modal-header-navidad">
+                <img src="{{ asset('img/Precisogps.png') }}" alt="Logo PrecisoGPS" class="logo-navidad">
+                <button class="close-navidad" onclick="cerrarModal()">×</button>
+            </div>
+            <div class="modal-body-navidad">
+                <img src="{{ asset('img/MariKari.jpg') }}" alt="Imagen navideña" class="imagen-navidad">
+                <p class="mensaje-navidad">
+                    <b>Todo detalle unido al amor y confianza hacen una gran navidad.</b>
+                    Esta fecha nos permite agradecer su fidelidad y nos inspira a dar lo mejor de nosotros cada día. En
+                    el 2025,
+                    <b>ratificamos nuestro compromiso de mejora continua e innovación,</b> seguiremos acompañando su
+                    ruta de
+                    viaje y
+                    esperamos que la carretera de su vida esté llena de logros, éxitos y felicidad.
+                </p>
+                <h4>FELIZ NAVIDAD</h4>
+                <p>PrecisoGPS<br>Justo en el Punto</p>
+            </div>
+        </div>
+    </div>
+    <script>
+        // Mostrar el modal automáticamente al cargar la página
+        window.addEventListener('DOMContentLoaded', () => {
+            const modal = document.getElementById('modalNavidad');
+            modal.style.display = 'flex';
+        });
+
+        function mostrarModal() {
+            const modal = document.querySelector('.modal-navidad');
+            modal.style.display = 'flex';
+        }
+
+        function cerrarModal() {
+            const modal = document.querySelector('.modal-navidad');
+            modal.style.display = 'none';
+        }
+
+        // Cierra el modal al hacer clic fuera de él
+        window.addEventListener('click', function(event) {
+            const modal = document.querySelector('.modal-navidad');
+            if (event.target === modal) {
+                cerrarModal();
+            }
+        });
+    </script>
+
 @endsection
 @section('jsCode', 'js/scripts.js')
--- back/resources/views/layout.blade.php
+++ back/resources/views/layout.blade.php
@@ -175,58 +175,6 @@
         });
     </script>
 
-    <div class="modal-navidad">
-        <div class="modal-content-navidad">
-            <div class="modal-header-navidad">
-                <img src="{{ asset('img/Precisogps.png') }}" alt="Logo PrecisoGPS" class="logo-navidad">
-                <button class="close-navidad" onclick="cerrarModal()">×</button>
-            </div>
-            <div class="modal-body-navidad">
-                <img src="{{ asset('img/MariKari.jpg') }}" alt="Imagen navideña" class="imagen-navidad">
-                <p class="mensaje-navidad">
-                    <b>Todo detalle unido al amor y confianza hacen una gran navidad.</b>
-                    Esta fecha nos permite agradecer su fidelidad y nos inspira a dar lo mejor de nosotros cada día. En
-                    el 2025,
-                    <b>ratificamos nuestro compromiso de mejora continua e innovación,</b> seguiremos acompañando su
-                    ruta de
-                    viaje y
-                    esperamos que la carretera de su vida esté llena de logros, éxitos y felicidad.
-                </p>
-                <h4>FELIZ NAVIDAD</h4>
-                <p>PrecisoGPS<br>Justo en el Punto</p>
-            </div>
-        </div>
-    </div>
-    <script>
-        // Mostrar el modal automáticamente al cargar la página
-        window.addEventListener('DOMContentLoaded', () => {
-            const modal = document.getElementById('modalNavidad');
-            modal.style.display = 'flex';
-        });
-
-        function mostrarModal() {
-            const modal = document.querySelector('.modal-navidad');
-            modal.style.display = 'flex';
-        }
-
-        function cerrarModal() {
-            const modal = document.querySelector('.modal-navidad');
-            modal.style.display = 'none';
-        }
-
-        // Cierra el modal al hacer clic fuera de él
-        window.addEventListener('click', function(event) {
-            const modal = document.querySelector('.modal-navidad');
-            if (event.target === modal) {
-                cerrarModal();
-            }
-        });
-    </script>
-
-
-
-
-
 </body>
 
 </html>
