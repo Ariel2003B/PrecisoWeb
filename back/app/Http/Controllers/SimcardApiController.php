70f8a340fbbf5aad445909f7e8985652fee3aec1
Elvis Guato <elvisguato02@gmail.com>
2024-12-27 23:38:41 -0500

    paginacion en api

--- back/app/Http/Controllers/SimcardApiController.php
+++ back/app/Http/Controllers/SimcardApiController.php
@@ -9,13 +9,13 @@ class SimcardApiController extends Controller
 {
     public function index(Request $request)
     {
-        // Crear la consulta base con relaciones
+        // Crear la consulta base
         $query = SIMCARD::without('v_e_h_i_c_u_l_o');
-
+    
         // Aplicar filtro de búsqueda
         if ($request->filled('search')) {
             $search = $request->input('search');
-
+    
             $query->where(function ($q) use ($search) {
                 $q->where('PROPIETARIO', 'like', "%$search%")
                     ->orWhere('CUENTA', 'like', "%$search%")
@@ -25,9 +25,8 @@ class SimcardApiController extends Controller
                     ->orWhere('NUMEROTELEFONO', 'like', "%$search%")
                     ->orWhere('ESTADO', 'like', "%$search%")
                     ->orWhere('GRUPO', 'like', "%$search%")
-                    ->orWhere('ASIGNACION', 'like', "%$search%")
-                ;
-
+                    ->orWhere('ASIGNACION', 'like', "%$search%");
+                
                 // Manejar el caso de "Sin Asignar"
                 if (strtolower($search) === 'sin asignar' || strtolower($search) === 'asignar' || strtolower($search) === 'sin') {
                     $q->whereNull('GRUPO')
@@ -35,16 +34,26 @@ class SimcardApiController extends Controller
                 }
             });
         }
-
+    
         // Ordenar los resultados
         $query->orderBy('ID_SIM', 'desc');
-
-        // Obtener todos los registros sin paginación
-        $simcards = $query->get();
-
+    
+        // Aplicar paginación o límite de registros
+        if ($request->filled('page') && $request->filled('pageSize')) {
+            // Paginación
+            $simcards = $query->paginate($request->input('pageSize'));
+        } elseif ($request->filled('limit')) {
+            // Límite de registros
+            $simcards = $query->limit($request->input('limit'))->get();
+        } else {
+            // Obtener todos los registros sin paginación
+            $simcards = $query->get();
+        }
+    
         // Retornar los datos en formato JSON
         return response()->json($simcards);
     }
+    
 
 
 
