535e20bc560dbd1ce66db3c52cf468c19f4536df
Elvis Guato <elvisguato02@gmail.com>
2025-01-27 08:39:33 -0500

    permiso solo lectura

--- back/resources/views/simcard/index.blade.php
+++ back/resources/views/simcard/index.blade.php
@@ -119,10 +119,14 @@
                                         @endif
                                     </td>
                                     <td>
-                                        <a href="{{ route('simcards.edit', $simcard->ID_SIM) }}"
-                                            class="btn btn-outline-primary btn-sm">
-                                            <i class="fas fa-edit"></i> Editar
-                                        </a>
+                                        @if (!Auth::check())
+                                            @if (!Auth::user()->p_e_r_f_i_l->p_e_r_m_i_s_o_s->contains('DESCRIPCION', 'LECTURA'))
+                                                <a href="{{ route('simcards.edit', $simcard->ID_SIM) }}"
+                                                    class="btn btn-outline-primary btn-sm">
+                                                    <i class="fas fa-edit"></i> Editar
+                                                </a>
+                                            @endif
+                                        @endif
                                     </td>
                                 </tr>
                             @endforeach
