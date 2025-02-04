c998fba27a38858a915c2d47b9ccfe4e74dff79c
Elvis Guato <elvisguato02@gmail.com>
2025-02-03 22:37:45 -0500

    prueba final 6

--- back/app/Console/Kernel.php
+++ back/app/Console/Kernel.php
@@ -2,7 +2,6 @@
 
 namespace App\Console;
 
-use App\Http\Controllers\SimCardController;
 use App\Jobs\UpdateWialonPhones;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -14,25 +13,22 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      */
+
     protected function schedule(Schedule $schedule): void
     {
         $schedule->call(function () {
-            try {
-                $controller = app(SimCardController::class);
-                $controller->updateWialonPhones(new \Illuminate\Http\Request());
-                Log::info("✅ Tarea programada ejecutada correctamente.");
-            } catch (\Exception $e) {
-                Log::error("❌ Error en tarea programada: " . $e->getMessage());
-            }
-        })->dailyAt('03:24');// Se ejecuta todos los días a las 2:00 AM
+            $controller = new \App\Http\Controllers\SimCardController();
+            $controller->updateWialonPhones(new \Illuminate\Http\Request());
+        })->dailyAt('03:39');
     }
+
 
     /**
      * Register the commands for the application.
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
