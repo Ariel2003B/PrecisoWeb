5707f92824f6712394a81d33cf49827d4e938bdb
Elvis Guato <elvisguato02@gmail.com>
2025-03-28 15:05:14 -0500

    diseno del pdf 5

--- back/resources/views/home/plataformas.blade.php
+++ back/resources/views/home/plataformas.blade.php
@@ -100,7 +100,7 @@
                             <h4 class="my-3">ACCESO CLIENTES</h4>
                             <a class="btn btn-success" href="{{ route('login.form') }}">Iniciar sesion</a>
                         </div>
-                    @else   
+                    @else
                         @if (Auth::user()->p_e_r_f_i_l->p_e_r_m_i_s_o_s->contains('DESCRIPCION', 'SIMCARDS'))
                             <div class="col-md-4 text-center">
                                 <span class="fa-stack fa-4x">
@@ -259,16 +259,17 @@
                             </div>
                         @endif
                         {{-- <i class="fa-solid fa-blog"></i> --}}
-
-                        <div class="col-md-4">
-                            <span class="fa-stack fa-4x">
-                                <i class="fas fa-solid fa-square fa-stack-2x text-primary"></i>
-                                <i class="fa-solid fa-person fa-stack-1x fa-inverse"></i>
-                            </span>
-                            <h4 class="my-3">REGISTRAR CONTEO PASAJEROS</h4>
-                            <a class="btn btn-success" href="{{ route('reportes.index') }}" target="_blank">Visitar
-                                página</a>
-                        </div>
+                        @if (Auth::user()->p_e_r_f_i_l->p_e_r_m_i_s_o_s->contains('DESCRIPCION', 'CONTEO'))
+                            <div class="col-md-4">
+                                <span class="fa-stack fa-4x">
+                                    <i class="fas fa-solid fa-square fa-stack-2x text-primary"></i>
+                                    <i class="fa-solid fa-person fa-stack-1x fa-inverse"></i>
+                                </span>
+                                <h4 class="my-3">REGISTRAR CONTEO PASAJEROS</h4>
+                                <a class="btn btn-success" href="{{ route('reportes.index') }}" target="_blank">Visitar
+                                    página</a>
+                            </div>
+                        @endif
                         <!-- Modal para WIALON -->
                         <div id="modalWialon"
                             class="custom-modal position-absolute bg-white border rounded shadow p-3 text-center"
