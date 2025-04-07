2b24cf1f6b87d1559e07d53b06e5687772c9166e
Elvis Guato <elvisguato02@gmail.com>
2025-04-07 18:25:26 -0500

    cambios en index 5

--- back/resources/views/partials/reporte_global.blade.php
+++ back/resources/views/partials/reporte_global.blade.php
@@ -13,14 +13,12 @@
                 <td>{{ $unidad }}</td>
                 <td>${{ number_format($datos['total_produccion'], 2) }}</td>
                 <td>{{ $datos['total_vueltas'] }}</td>
-                <td>{{ $datos['ultima_vuelta'] }}</td>
             </tr>
         @endforeach
         <tr class="table-success">
             <th>Total Global</th>
             <th>${{ number_format($totalGlobal, 2) }}</th>
             <th>{{ $totalVueltasGlobal }}</th>
-            <th>-</th>
         </tr>
     </tbody>
 </table>
