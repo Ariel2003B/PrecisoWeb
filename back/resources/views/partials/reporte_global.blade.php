01309a7afeaef50d12e6c8edd4cea7cda2161a04
Elvis Guato <elvisguato02@gmail.com>
2025-04-07 18:27:35 -0500

    cambios en index 6

--- back/resources/views/partials/reporte_global.blade.php
+++ back/resources/views/partials/reporte_global.blade.php
@@ -1,12 +1,14 @@
 <table class="table table-bordered text-center align-middle">
     <thead class="table-dark">
         <tr>
+            <th>Vueltas</th>
             <th>Unidad</th>
             <th>Total Producción ($)</th>
-            <th>Total Vueltas</th>
+            
         </tr>
     </thead>
     <tbody>
+        <th>{{ $totalVueltasGlobal }}</th>
         @foreach ($produccionPorUnidad as $unidad => $datos)
             <tr>
                 <td>{{ $unidad }}</td>
@@ -17,7 +19,7 @@
         <tr class="table-success">
             <th>Total Global</th>
             <th>${{ number_format($totalGlobal, 2) }}</th>
-            <th>{{ $totalVueltasGlobal }}</th>
+           
         </tr>
     </tbody>
 </table>
