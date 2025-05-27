b0952499923e0df277b56a9d6c40fbecc7b4d473
Elvis Guato <elvisguato02@gmail.com>
2025-05-27 09:51:46 -0500

    cambios de google

--- back/resources/views/layout.blade.php
+++ back/resources/views/layout.blade.php
@@ -40,9 +40,31 @@
   * Author: BootstrapMade.com
   * License: https://bootstrapmade.com/license/
   ======================================================== -->
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-N9QC5VKN');
+    </script>
+    <!-- End Google Tag Manager -->
 </head>
 
 <body class="index-page">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N9QC5VKN" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <header id="header" class="header d-flex align-items-center sticky-top">
         <div class="container position-relative d-flex align-items-center">
 
@@ -173,7 +195,7 @@
             class="bi bi-arrow-up-short"></i></a>
     <!-- Botón flotante de WhatsApp -->
     <!-- Botón flotante de WhatsApp con mensaje predefinido -->
-  
+
 
 
 
